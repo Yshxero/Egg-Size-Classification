f4a4f61f8afedad55ebb87a48167a3f9e62036d2
Yshxer0 <raysimonbantaculo@gmail.com>
2025-11-25 03:21:59 +0800

    Update

--- main.py
+++ main.py
@@ -56,3 +56,8 @@ async def predict(file: UploadFile = File(...)):
     size = cluster_to_size.get(cluster, "Unknown")
 
     return {"cluster": cluster, "size": size}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False)
